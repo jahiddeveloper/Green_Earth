f491e44e7930a2ecd315dd33692e1d7ce3ce4df3
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-11-18 14:51:51 +0600

    Load Cart is compleat

--- script.js
+++ script.js
@@ -10,6 +10,69 @@ let loadCategory = () => {
         })
 }
 
+
+// Load Cart 
+
+let loadCart = () => {
+    fetch("https://openapi.programming-hero.com/api/plants")
+        .then(res => res.json())
+        .then(json => {
+            // console.log(json.plants);
+            displayCart(json.plants);
+        })
+}
+
+let displayCart = (plants) => {
+
+    let cartContiner = document.getElementById("cart-container");
+
+    plants.forEach((plant) => {
+        let div = document.createElement("div");
+        div.innerHTML = `
+            <div
+                class="card bg-base-100 w-94 shadow-lg mx-auto md:mx-0 block"
+              >
+                <figure>
+                  <img
+                    src="${plant.image}"
+                    alt="Shoes"
+                    class="p-2 h-55 w-full object-cover"
+                  />
+                </figure>
+                <div class="card-body">
+                  <h2 class="text-xl font-bold">${plant.name}</h2>
+                  <p>
+                    ${plant.description}
+                  </p>
+
+                  <div class="flex justify-between items-center mt-3">
+                    <div
+                      class="text-[#15803d] px-5 py-2 bg-[#dcfce7] rounded-3xl"
+                    >
+                      <p>${plant.category}</p>
+                    </div>
+                    <div class="font-bold text-lg">
+                      <p>৳ <span>${plant.price}</span></p>
+                    </div>
+                  </div>
+
+                  <div class="card-actions justify-center mt-3">
+                    <button
+                      class="btn btn-primary w-full bg-[#15803d] border-none"
+                    >
+                      Add To Card
+                    </button>
+                  </div>
+                </div>
+              </div>
+        `
+        cartContiner.append(div);
+    })
+}
+
+loadCart();
+
+
 let displayCategory = (categories) => {
 
     let categoryContainer = document.getElementById("category-container");
